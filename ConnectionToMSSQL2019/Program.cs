26876d316171a84d4b2fc88b3cae467f176d2f35
knoerregaard <knoerregaard@gmail.com>
2020-11-06 09:59:04 +0100

    Add project files.

---
+++ ConnectionToMSSQL2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConnectionToMSSQL2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connected");
+        }
+    }
+}
